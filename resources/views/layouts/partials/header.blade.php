16d83d933dabbe83106b75e110ffff9fbe17ec01
djiogolucrece <lucredjiogo@gmail.com>
2023-08-19 08:22:07 +0100

    integration template client

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg  ftco_navbar ftco-navbar-light" id="ftco-navbar" style="background-color: red">
+    <div class="container">
+    <img class="navbar-brand" src="images/3ia logo-01.png" alt="" height="100px" width="240px">
+    {{-- <a class="navbar-brand" href="index-2.html"><i class="flaticon-university"></i>Genius <br><small>University</small></a> --}}
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="oi oi-menu"></span> Menu
+    </button>
+    <div class="collapse navbar-collapse" id="ftco-nav">
+    <ul class="navbar-nav ml-auto">
+    <li class="nav-item active"><a href="index-2.html" class="nav-link">Accueil</a></li>
+    <li class="nav-item"><a href="about.html" class="nav-link">Apropos</a></li>
+    <li class="nav-item"><a href="course.html" class="nav-link">Formations</a></li>
+    <li class="nav-item"><a href="teacher.html" class="nav-link">Formateurs</a></li>
+    <li class="nav-item"><a href="event.html" class="nav-link">Evènements</a></li>
+    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+    <li class="nav-item cta"><a href="contact.html" class="nav-link"><span>Apply Now!</span></a></li>
+    </ul>
+    </div>
+    </div>
+    </nav>
